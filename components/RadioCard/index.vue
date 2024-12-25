deaf76463f86428ac4585d16f4caeb19224ddfbb
haodd <1903289197@qq.com>
2024-12-25 11:57:56 +0800

    style: 通知管理-通知模板样式优化

--- components/RadioCard/index.vue
+++ components/RadioCard/index.vue
@@ -20,7 +20,7 @@
             @click="handleRadio(item)"
         >
             <img v-if="item.logo" class="img" :src="item.logo" alt="" />
-            <span>{{ item.label }}</span>
+            <span style="white-space: nowrap">{{ item.label }}</span>
             <div
                 :class="[
                     'checked-icon',
